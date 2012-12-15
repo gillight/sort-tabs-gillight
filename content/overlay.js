cc8a3eee91ad92931fffd60a1da966912c3d4727
gillight <gillight@george.(none)>
2012-12-15 23:25:02 +0800

    新增 query 比較功能

--- content/overlay.js
+++ content/overlay.js
@@ -27,14 +27,7 @@ var SortTabs = {
 	  }
   },
   
-  cmp_domain: function(a, b){
-	  if(a.toLocaleLowerCase() == b.toLocaleLowerCase()){
-		  return 0;
-	  }
-	  var a_dc_arr = a.split('.');
-	  var b_dc_arr = b.split('.');
-	  a_dc_arr.reverse();
-	  b_dc_arr.reverse();
+  cmp_array: function(a_dc_arr, b_dc_arr){
 	  var BreakException ={};
 	  var ret = 0;
 	  try{
@@ -43,7 +36,7 @@ var SortTabs = {
 				  ret = 1;
 				  throw BreakException;
 			  }
-			  var cmp = a_dc.toLocaleLowerCase().localeCompare(b_dc_arr[a_i].toLocaleLowerCase());
+			  var cmp = a_dc.localeCompare(b_dc_arr[a_i]);
 			  if(cmp != 0){
 				  ret = cmp;
 				  throw BreakException;
@@ -56,6 +49,19 @@ var SortTabs = {
 	  return -1;
   },
   
+  cmp_domain: function(a, b){
+	  var a_lc = a.toLocaleLowerCase();
+	  var b_lc = b.toLocaleLowerCase();
+	  if(a_lc == b_lc){
+		  return 0;
+	  }
+	  var a_dc_arr = a_lc.split('.');
+	  var b_dc_arr = b_lc.split('.');
+	  a_dc_arr.reverse();
+	  b_dc_arr.reverse();
+	  return SortTabs.cmp_array(a_dc_arr, b_dc_arr);
+  },
+  
   get_host: function(aURI){
 	  var host = '';
 	  try{
@@ -63,13 +69,80 @@ var SortTabs = {
 	  }catch(e){}
 	  return host;
   },
+  
+  cast_URI_to_URL: function(aURI){
+	try {
+	  return aURI.QueryInterface(Components.interfaces.nsIURL);
+	}
+	catch(e){
+	  return null;
+	}	  
+  },
+  
+  decode_query: function(query){
+	  var pair_split = query.split('&');
+	  var pairs = {};
+	  pair_split.forEach(function(item){
+		  var temp = item.split('=');
+		  if(temp.length > 1){
+			pairs[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+		  }else{
+			pairs[decodeURIComponent(temp[0])] = '';
+		  }
+	  });
+	  var props = [];
+	  for(prop in pairs){
+		  SortTabs.log("prop:"+prop+" val:"+pairs[prop]);
+		  props[props.length] = prop;
+	  }
+	  props.sort();
+	  var ret = [];
+	  props.forEach(function(item){
+		  ret[ret.length] = item;
+		  ret[ret.length] = pairs[item];
+	  });
+	  ret.forEach(function(item){
+		  SortTabs.log("item: "+item);
+	  });
+	  return ret;
+  },
+  
+  cmp_query: function(a, b){
+	var a_decode = SortTabs.decode_query(a);
+	var b_decode = SortTabs.decode_query(b);
+	return SortTabs.cmp_array(a_decode, b_decode);
+  },
+  
+  cmp_URL: function(a, b){
+	  SortTabs.log(a.filePath);
+	  SortTabs.log(b.filePath);
+	  var cmp_path = a.filePath.localeCompare(b.filePath);
+	  if(cmp_path !=0 ) { return cmp_path; }
+	  SortTabs.log(a.query);
+	  SortTabs.log(b.query);
+	  var cmp_query = SortTabs.cmp_query(a.query, b.query);
+	  if(cmp_query !=0 ) { return cmp_query; }
+	  SortTabs.log(a.ref);
+	  SortTabs.log(b.ref);
+	  return  a.ref.localeCompare(b.ref);
+  },
 
   domain_sort: function(a, b){
 	  SortTabs.log(a.label);
 	  SortTabs.log(b.label);
 	  var cmp = SortTabs.cmp_domain(SortTabs.get_host(a.linkedBrowser.currentURI), SortTabs.get_host(b.linkedBrowser.currentURI));
 	  if(cmp != 0){ return cmp; }
-	  return a.linkedBrowser.currentURI.path.localeCompare(b.linkedBrowser.currentURI.path);
+	  var a_URL = SortTabs.cast_URI_to_URL(a.linkedBrowser.currentURI);
+	  var b_URL = SortTabs.cast_URI_to_URL(b.linkedBrowser.currentURI);
+	  if(a_URL != null && b_URL != null){
+		  return SortTabs.cmp_URL(a_URL, b_URL);
+	  }else if(a_URL == null && b_URL != null){
+		  return -1;
+	  }else if(a_URL != null && b_URL == null){
+		  return 1;
+	  }else{
+		return a.linkedBrowser.currentURI.path.localeCompare(b.linkedBrowser.currentURI.path);
+	  }
   },
   
   log: function(m){
